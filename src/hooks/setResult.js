0824c3f3808571c9fd632be7050268b72812db26
Dr-hasan001 <drha2757@gmail.com>
2024-07-11 11:53:45 +0300

    Update setResult.js

--- src/hooks/setResult.js
+++ src/hooks/setResult.js
@@ -23,7 +23,7 @@ export const usePublishResult = (resultData) => {
     try {
       if (!username) throw new Error("Couldn't get Result");
       await postServerData(
-        `$https://cisquizapp.onrender.com/api/result`,
+        `https://cisquizapp.onrender.com/api/result`,
         resultData,
         (data) => data
       );
