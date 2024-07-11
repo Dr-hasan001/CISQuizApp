c32284bd4deb0195fd6b5835ebe0765a2d049ea8
Dr-hasan001 <drha2757@gmail.com>
2024-07-11 12:06:21 +0300

    Update FetchQuestion.js

--- src/hooks/FetchQuestion.js
+++ src/hooks/FetchQuestion.js
@@ -21,7 +21,7 @@ export const useFetchQuestion = () => {
     (async () => {
       try {
         const [{ questions, answers }] = await getServerData(
-          `https://cisquizapp-server.onrender.com`,
+          `https://cisquizapp-server.onrender.com/api/questions`,
           (data) => data
         );
 
